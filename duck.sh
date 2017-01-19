f65874dfc5fd1e25d500931d14a9bce8f5a3b116
John Skilbeck <john.skilbeck@gmail.com>
2017-01-19 01:18:32 -0800

    wasnt working

--- duck.sh
+++ duck.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+set -e
 
 api_token=${duckdns}
 
-echo url="https://www.duckdns.org/update?domains=skilbjo&token=$api_token=" | \
+echo url="https://www.duckdns.org/update?domains=skilbjo&token=${api_token}&ip=" | \
   curl -k -o ./duck.log -K -
